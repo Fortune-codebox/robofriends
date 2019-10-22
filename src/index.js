7c91925a2482cef112b80d613bfa5f0c022d1cba
Uzoma Fortune <izzyuzoma@gmail.com>
2019-10-22 23:26:10 +0100

    first components and addition of robots.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(
+            <div>
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} image={robots[0].img_url}/>
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} image={robots[1].img_url}/>
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} image={robots[2].img_url}/>
+            </div>, 
+            document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Mary Slessor',
+        username: 'MarySlex',
+        email: 'mary.slessor@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        username: 'Janey01',
+        email: 'jane.doe@gmail.com'
+    },
+    {
+        id:3,
+        name: 'Thomas Morgan',
+        username: 'Thomasmanny',
+        email: 'thomas.morgaon01@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Mathew James',
+        username: 'mathew01',
+        email: 'thomas.mathew@yahoo.com'
+    },
+    {
+        id: 5,
+        name: 'John mathias',
+        username: 'Jonny',
+        email: 'john.matty@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Linda Someday',
+        username: 'Lindy7',
+        email: 'lindysome01@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'James through',
+        username: 'jammes001',
+        email: 'jamesthrough002@yandex.com'
+    },
+    {
+        id: 8,
+        name: 'Amanda Perry',
+        username: 'ama01',
+        email: 'amanda.perry09@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Mary Mary',
+        username: 'marrrrrrrr01',
+        email: 'mary.mary@yahoo.com'
+    },
+    {
+        id: 10,
+        name: 'Jerry Gbana',
+        username: 'jerry01',
+        email: 'jery.gbana@yahoo.com'
+    }
+]
