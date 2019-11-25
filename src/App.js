c622d5f0fd7c045a8a39defe2bb6f2c0a4cb6b94
Uzoma Fortune <izzyuzoma@gmail.com>
2019-11-25 06:53:01 +0100

    Pushing Updates on Scroll.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+import Scroll from './Scroll';
+// import {robots} from './robots';
 
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+       
     }
 
     onSearchChange = (event) => {
@@ -21,13 +29,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
         return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
+
+        if(this.state.robots.length === 0) {
+            return <h1 className="tc">Loading...</h1>
+        } else {
         return (
             <div className='tc'>
             <h1>RoboFriends</h1>
             <SearchBox searchChange= {this.onSearchChange}/>
+            <Scroll>
             <CardList robots={filteredRobots}/>
+            </Scroll>
             </div> 
         );
+        }
     }
     
 }
---
+++ src/Scroll.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+ const Scroll = (props) => {
+     console.log(props);
+     return(
+         <div></div>
+     );
+ }
+
+
+
+
+
+ export default Scroll;
